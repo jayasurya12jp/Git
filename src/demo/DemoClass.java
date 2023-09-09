1e7b961a3a87d821765f48b34ed4bcc3fdc94423
jayasurya <jayasuryakannimari554@gmail.com>
2023-09-09 09:46:19 +0530

    second

--- src/demo/DemoClass.java
+++ src/demo/DemoClass.java
@@ -4,8 +4,12 @@ public class DemoClass {
 
 	public static void main(String[] args) {
 		for (int i=0;i<=10;i++) {
-			System.out.println(i);
+			for (int j=0;j<=i;j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
 		}
+		
 	}
 
 }
